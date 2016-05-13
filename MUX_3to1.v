8ad56957f39db0f7c35f723b04fa051245959b0c
Kevin Chen <me@kevchentw.com>
2016-05-13 22:39:58 +0800

    first commit

---
+++ MUX_3to1.v
@@ -0,0 +1,34 @@
+//Subject:     CO project 2 - MUX 221
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:
+//----------------------------------------------
+//Date:
+//----------------------------------------------
+//Description:
+//--------------------------------------------------------------------------------
+
+module MUX_3to1(
+               data0_i,
+               data1_i,
+               data2_i,
+               select_i,
+               data_o
+               );
+
+parameter size = 0;
+
+//I/O ports
+input   [size-1:0] data0_i;
+input   [size-1:0] data1_i;
+input   [1:0]      select_i;
+output  [size-1:0] data_o;
+
+//Internal Signals
+wire     [size-1:0] data_o;
+
+//Main function
+assign data_o = (select_i == 2'b00 ? data0_i : (select_i == 2'b01 ? data1_i : data2_i));
+
+endmodule
